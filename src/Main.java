fc281dd119eb2e31d69f4d3bc974548ceac8168c
Vladislav KIm <114429364+meowqe@users.noreply.github.com>
2022-09-30 21:56:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        int nn,i;
-        m:for (i=1; i<=9; ++i)
-        {
-            nn=n;
-            while(nn>0)
-            {
-                if (nn%10==i)
-                {
+        int n = in.nextInt();
+        if (n == 0) System.out.println(0);
+        m:
+        for (int i = 1; i <= 9; ++i) {
+            int a = n;
+            while (n > 0) {
+                if (n % 10 == i){
                     System.out.println(i);
                     break m;
                 }
-                nn=nn/10;
+                n /= 10;
+
+
             }
         }
+
+
     }
 }
