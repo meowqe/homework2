08ee27865cd1251896a80766ba64f25b2e93256e
Vladislav KIm <114429364+meowqe@users.noreply.github.com>
2022-09-30 22:39:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,30 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int x = n%10;
-        String a = String.valueOf(n);
-        switch (x) {
+        String a = String.valueOf(n + " " + "tort");
+        if (n / 1000 >= 11 && n / 1000 <= 14) {
+            a += "ov";
+        } switch (x) {
             case 1:
-                a = "TORT";
+                a += "";
                 break;
             case 2:
             case 3:
             case 4:
-                a = "TORTA";
+                a += "a";
                 break;
-            case 0:
-            case 5:
-            case 6 & 7:
-                a = "TORTOV";
+            default:
+                a += "ov";
                 break;
-            case 8 & 9:
-                a = "TORTOV";
+
+
+
+            }
+         System.out.println(a);
+
 
 
 
         }
-        System.out.println(a);
+
     }
-}
