79fc9ee2aae8013b4b7393aec7481abea4767303
Vladislav KIm <114429364+meowqe@users.noreply.github.com>
2022-10-01 12:22:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int x = n%10;
-        String a = String.valueOf(n + " " + "tort");
-        if (n / 1000 >= 11 && n / 1000 <= 14) {
-            a += "ov";
-        } switch (x) {
-            case 1:
-                a += "";
-                break;
-            case 2:
-            case 3:
-            case 4:
-                a += "a";
-                break;
-            default:
-                a += "ov";
-                break;
+        int a = 11;
+        int b = 2;
+        int nyam = 0;
+        int x;
 
+        for (x = 1; x < n; ++x) {
+            if (x % 2 != 0) {
+                a = a *3;
+                System.out.println(a);
+
+
+
+            } else if (x % 2 == 0) {
+                a -= 10 * b;
+                nyam +=10 * b;
 
 
             }
-         System.out.println(a);
+            if (nyam>=70) {
+               b += 1;
+               nyam -= 70;
+           }
+        }   System.out.println(String.valueOf(a) + " " + String.valueOf(b)+ " " + String.valueOf(nyam));
 
-
-
-
-        }
 
     }
+}
+
+
+
